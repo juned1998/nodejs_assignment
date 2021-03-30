284b218d6e6cfdb10fc773ff16bc0c795275e50d
Juned Khan <junaidkhanhere@gmail.com>
2021-03-30 22:14:24 +0530

    database

--- db.js
+++ db.js
@@ -5,20 +5,22 @@ dotenv.config({ path: "./config.env" });
 // Sequelize
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(
-    process.env.DBNAME,
-    process.env.DBUSER,
-    process.env.DBPASSWORD,
-    {
-        host: process.env.DBHOST,
-        dialect: "postgres",
-        pool: {
-            max: 5,
-            min: 0,
-            idle: 10000,
-        },
-    }
-);
+// const sequelize = new Sequelize(
+//     process.env.DBNAME,
+//     process.env.DBUSER,
+//     process.env.DBPASSWORD,
+//     {
+//         host: process.env.DBHOST,
+//         dialect: "postgres",
+//         pool: {
+//             max: 5,
+//             min: 0,
+//             idle: 10000,
+//         },
+//     }
+// );
+
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 // console.log(sequelize);
 
